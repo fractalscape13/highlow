745a9d8edaefaac2d9015d95d314a72284d9c086
Jamison Cozart and Joseph Wangemann <joseph@>
2020-02-24 15:21:38 -0800

    Refactor to use Binary Sort algorithm to determine user number

--- HighLow.cs
+++ HighLow.cs
@@ -2,34 +2,28 @@ using System;
 
 class Program
 {
-  public static int currentNum = 50;
+  public static int high = 101;
+  public static int low = 1;
   public static void Main()
   {
+    int currentNum = (high + low)/2;
     Console.WriteLine("Pick a number between 1 and 100, don't tell me it...");
     Console.WriteLine("Is your number higher or lower than " + currentNum + "? (Higher/Lower/Correct)");
     string userAnswer = Console.ReadLine();
     if (userAnswer == "Higher")
     {
-      Higher(currentNum);
+      low = currentNum;
       Main();
     }
     else if (userAnswer == "Lower")
     {
-      Lower(currentNum);
+      high = currentNum;
       Main();
     }
     else if (userAnswer == "Correct")
     {
-      Console.WriteLine("Your number was" + currentNum);
+      Console.WriteLine("Your number was " + currentNum);
     }
 
-  }
-  public static void Higher(int inputNum)
-  {
-    currentNum += ((100 - inputNum) / 2);
-  }
-  public static void Lower(int inputNum)
-  {
-    currentNum -= (inputNum / 2);
   }
 }
